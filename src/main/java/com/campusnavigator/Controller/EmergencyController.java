6b527a661a062a638da09850d411e982ec13d4b8
MasuRii <masurii@users.noreply.github.com>
2026-05-22 01:32:23 +0800

    fix(api): scope emergency routes under /api/emergency

--- src/main/java/com/campusnavigator/Controller/EmergencyController.java
+++ src/main/java/com/campusnavigator/Controller/EmergencyController.java
@@ -3,23 +3,21 @@ package com.campusnavigator.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.campusnavigator.Entity.EmergencyContacts;
 import com.campusnavigator.Service.EmergencyService;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-
-
-
 @RestController
+@RequestMapping("/api/emergency")
 public class EmergencyController {
     @Autowired
     EmergencyService eserv;
@@ -32,32 +30,24 @@ public class EmergencyController {
 
     ///CREATE
     @PostMapping("/postContacts")
-    public EmergencyContacts postContactRecord(@RequestBody EmergencyContacts emergency) 
-    {     
+    public EmergencyContacts postContactRecord(@RequestBody EmergencyContacts emergency) {
         return eserv.postContacts(emergency);
     }
 
     //READ
     @GetMapping("/getAllContacts")
-    public List<EmergencyContacts> getContacts()
-    {
+    public List<EmergencyContacts> getContacts() {
         return eserv.getAllContacts();
     }
 
     //UPDATE
     @PutMapping("/putContactRecord")
     public EmergencyContacts putContacts(@RequestParam int contactID, @RequestBody EmergencyContacts newEmergencyContacts) {
-        //TODO: process PUT request
-        
         return eserv.putContacts(contactID, newEmergencyContacts);
     }
 
     @DeleteMapping("/deleteContacts/{contactID}")
-    public String deleteContactRecord(@PathVariable int contactID)
-    {
+    public String deleteContactRecord(@PathVariable int contactID) {
         return eserv.deleteContacts(contactID);
     }
-    
-    
-    
 }
