abce07cec6feaa80308d713cb1a635752f1fade4
MasuRii Math Lee マス・リー <kanjiharigana@gmail.com>
2026-05-21 21:22:39 +0800

    Update UserController with BCrypt login, CORS from config

--- src/main/java/com/campusnavigator/Controller/UserController.java
+++ src/main/java/com/campusnavigator/Controller/UserController.java
@@ -2,13 +2,14 @@ package com.campusnavigator.Controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.campusnavigator.Entity.User;
 import com.campusnavigator.Service.UserService;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,7 +21,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 @RequestMapping("/api/user")
-@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 public class UserController {
 
     @Autowired
@@ -37,6 +37,26 @@ public class UserController {
     public User postUserRecord(@RequestBody User search)
     {
         return userv.postUserRecord(search);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<User> login(@RequestBody User loginRequest) {
+        User authenticatedUser = userv.authenticateUser(loginRequest.getEmail(), loginRequest.getPassword());
+        if (authenticatedUser == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        return ResponseEntity.ok(authenticatedUser);
+    }
+
+    @PostMapping("/admin/login")
+    public ResponseEntity<User> adminLogin(@RequestBody User loginRequest) {
+        User authenticatedAdmin = userv.authenticateAdmin(loginRequest.getEmail(), loginRequest.getPassword());
+        if (authenticatedAdmin == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        return ResponseEntity.ok(authenticatedAdmin);
     }
     //read
     @GetMapping("/getAllSearch")
